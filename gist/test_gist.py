3e49d2c60da24d009e7e34d1b78ccb8ff5cae7ad
jewon119 <jewon119@gmail.com>
2023-05-09 15:10:49 +0900

    Add: using skip in pytest

--- gist/test_gist.py
+++ gist/test_gist.py
@@ -1,2 +1,15 @@
+import pytest
+
+
 def test_our_first_test() -> None:
     assert 1 == 1
+
+
+@pytest.mark.skip
+def test_should_be_skipped() -> None:
+    assert 1 == 2
+
+
+@pytest.mark.skipif(4 > 1, reason="Skipped because 4 > 1")
+def test_should_be_skipped_if() -> None:
+    assert 1 == 2
