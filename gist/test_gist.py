2f36b7de8ce93edc11da8369df1d54e21a6d4f84
jewon119 <jewon119@gmail.com>
2023-05-09 15:32:36 +0900

    Add: xfail & slow test

--- gist/test_gist.py
+++ gist/test_gist.py
@@ -13,3 +13,20 @@ def test_should_be_skipped() -> None:
 @pytest.mark.skipif(4 > 1, reason="Skipped because 4 > 1")
 def test_should_be_skipped_if() -> None:
     assert 1 == 2
+
+
+@pytest.mark.xfail
+def test_done_care_if_fails() -> None:
+    assert 1 == 2
+
+
+@pytest.mark.slow
+def test_with_custom_mark1() -> None:
+    pass
+
+
+@pytest.mark.slow
+def test_with_custom_mark2() -> None:
+    pass
+
+
